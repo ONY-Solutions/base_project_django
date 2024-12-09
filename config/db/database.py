8a657c7a647b61066e1a353a979a635d442ca9c9
nleea <neldecas12@gmail.com>
2024-12-08 19:54:39 -0500

    change host db

--- config/db/database.py
+++ config/db/database.py
@@ -5,7 +5,7 @@ env = environ.Env()
 DB_DEVELOP_NAME = env.str("DB_NAME", "")
 DB_DEVELOP_USER = env.str("DB_USER", "")
 DB_DEVELOP_PASSWORD = env.str("DB_PASSWORD", "")
-DB_DEVELOP_HOST = env.str("DB_HOST", "")
+DB_DEVELOP_HOST = env.str("DB_HOST", "postgres")
 DB_DEVELOP_PORT = env.str("DB_PORT", "")
 
 DATABASE_CONFIG = {
