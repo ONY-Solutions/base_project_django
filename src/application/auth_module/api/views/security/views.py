c89a162bfa2a9341d9e451b59e80cf9fbe97a566
nleea <neldecas12@gmail.com>
2024-12-10 11:14:28 -0500

    optimizing image docker

--- src/application/auth_module/api/views/security/views.py
+++ src/application/auth_module/api/views/security/views.py
@@ -1,6 +1,6 @@
 # views.py
 
-from rest_framework import viewsets, status
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
